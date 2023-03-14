aca1b1623a04289279a54e792d52e6ee3b4570b2
dv-jamie <sjm2506@gmail.com>
2023-03-14 09:05:30 +0900

    냉장고 조회 dto 수정

--- src/models/refrigerators/dto/get-refrigerators.dto.ts
+++ src/models/refrigerators/dto/get-refrigerators.dto.ts
@@ -1,8 +1,10 @@
 import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 export class GetRefrigeratorsDto extends PaginationDto {
     @IsOptional()
+    @Type(() => Number)
     @IsNumber()
     categoryId: number
 
