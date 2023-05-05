fe0e71635fa735e76228672ac6bae1672594fd15
dv-jamie <sjm2506@gmail.com>
2023-05-05 20:15:40 +0900

    cors 수정

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-    })
+    }),
   );
   await app.listen(3001);
 }
