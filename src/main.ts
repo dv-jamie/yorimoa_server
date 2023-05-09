b77766e8d03d43147e11b8d9a7566ac974304da8
dv-jamie <sjm2506@gmail.com>
2023-05-10 03:32:47 +0900

    cors 옵션 수정

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.CLIENT_URL,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
